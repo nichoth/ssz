6cb82686c36ba6506bdb47d55a5a6420b998df3e
nichoth <nichoth@gmail.com>
2021-11-15 00:01:26 -0800

    more stuff

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const Libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
 const { NOISE } = require('libp2p-noise')
+const MPLEX = require('libp2p-mplex')
+const multiaddr = require('multiaddr')
 
-const node = await Libp2p.create({
-    modules: {
-        transport: [TCP],
-        connEncryption: [NOISE]
-    }
-})
+const main = async () => {
+    const node = await Libp2p.create({
+        addresses: {
+            // add a listen address (localhost) to accept TCP
+            // connections on a random port
+            listen: ['/ip4/127.0.0.1/tcp/0']
+        },
+        modules: {
+            transport: [TCP],
+            connEncryption: [NOISE],
+            streamMuxer: [MPLEX]
+        }
+    })
+
+    // start libp2p
+    await node.start()
+    console.log('libp2p has started')
+
+    // print out listening addresses
+    console.log('listening on addresses:')
+    node.multiaddrs.forEach(addr => {
+        console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`)
+    })
+
+    // stop libp2p
+    // await node.stop()
+    // console.log('libp2p has stopped')
+}
+
+main()
 
