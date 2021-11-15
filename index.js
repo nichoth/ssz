58fd38981b94c0d6c95a9a0ad2577ea8358ed04f
nichoth <nichoth@gmail.com>
2021-11-14 23:50:33 -0800

    add noise

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
+const { NOISE } = require('libp2p-noise')
 
 const node = await Libp2p.create({
     modules: {
-        transport: [TCP]
+        transport: [TCP],
+        connEncryption: [NOISE]
     }
 })
 
